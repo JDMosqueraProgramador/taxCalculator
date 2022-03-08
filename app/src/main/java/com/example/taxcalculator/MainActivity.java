8491a8203ecbf3909b0ac998fe6e38f6ce7b1daa
JDMosqueraProgramador <jdmm360@gmail.com>
2022-03-08 18:57:34 -0500

    First Commit

---
+++ app/src/main/java/com/example/taxcalculator/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.taxcalculator;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goTo(View view) {
+
+        Class cls = null;
+
+
+        switch(view.getId()) {
+            case R.id.btnPropietyTax:
+                //cls = RetentionActivity.class;
+                break;
+
+            case R.id.btnRetention:
+                cls = RetentionActivity.class;
+                break;
+
+            case R.id.btnVehicule:
+                //cls = RetentionActivity.class;
+                break;
+        }
+
+        if(cls != null) {
+            Intent intent = new Intent(this, cls);
+            startActivity(intent);
+        }
+
+    }
+}
