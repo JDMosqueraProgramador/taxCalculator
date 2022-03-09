fd337cfd7122eb8bfab413601197911a38f5eec5
JDMosqueraProgramador <jdmm360@gmail.com>
2022-03-08 19:08:44 -0500

    I Added all of activities

---
+++ app/src/main/java/com/example/taxcalculator/VehicleActivity.java
@@ -0,0 +1,13 @@
+package com.example.taxcalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VehicleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vehicle);
+    }
+}
